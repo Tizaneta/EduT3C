5b03f5ec9e6cbe549bbf27eedddb96aa4da9f5b9
Tiza <tizianoagustinrojas@gmail.com>
2026-04-26 22:37:25 -0300

    Se añadieron botones y pantalla extra al apretar un boton.

---
+++ edut3c/lib/screens/hardware_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class hardware_screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Hardware")),
+      body: Center(
+        child: Text("Pantalla de Hardware"),
+      ),
+    );
+  }
+}
--- edut3c/lib/screens/home.dart
+++ edut3c/lib/screens/home.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
+import 'hardware_screen.dart';
 class Home extends StatelessWidget {
 @override
 Widget build(BuildContext context) {
   return Scaffold(
     appBar: AppBar(
-      title: Text("EduT3C"),
+      title: Text("EduT3C")
     ),
     body: Center(
-      child: Text("esta es la pantalla de inicio, donde estaran ubicados los niveles."),
-    ),
+      child: Column(
+      children: [
+        ElevatedButton(onPressed: (){
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => hardware_screen()),
   );
-
-
+}, child: Text("Hardware")),
+        SizedBox(height: 30),
+        ElevatedButton(onPressed:(){print("Entraste a SO");}, child: Text("Sistemas Operativos")),
+        SizedBox(height: 30),
+        ElevatedButton(onPressed: (){print("Entraste a programacion");}, child: Text("Programación")),
+        SizedBox(height: 30),
+      ]
+      ),
+      ),
+  );
 }
 }
