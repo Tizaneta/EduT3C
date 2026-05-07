53daaa5dcb89953b1ce24b58b1552647ee0d13fe
Tiza <tizianoagustinrojas@gmail.com>
2026-05-07 15:49:51 -0300

    Actualizar inicios.dart

--- inicios.dart
+++ inicios.dart
@@ -123,6 +123,6 @@ void main() {
     print("El numero mas grande es: ${find_max(userNumbers)}");
     
 }
-//hola tiza se la come
+//hola tiza me puedo comer tu polla?
 
     // Nivel 3: Del nivel anterior, crear una función que diga cuál es el número más grande de la lista.
